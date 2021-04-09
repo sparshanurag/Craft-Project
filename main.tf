af333c889fe566d5eb826e1852abc4361571f41e
sparshanurag <30572979+sparshanurag@users.noreply.github.com>
2021-04-10 00:39:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -211,8 +211,8 @@ resource "aws_db_instance" "myrds" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   name                 = "wordpress"
-  username             = "karan"
-  password             = "karan12345"
+  username             = "sparsh"
+  password             = "sparsh1234"
   parameter_group_name = "default.mysql5.7"
   vpc_security_group_ids = [aws_security_group.wordpresstask-sg.id]
 
